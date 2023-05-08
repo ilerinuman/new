5a8c8fb5e2a7561c6a283741c33ac96e955edcde
Numan İleri <numanileri@gmail.com>
2023-05-08 12:56:48 +0300

    V3

--- src/main/java/class1.java
+++ src/main/java/class1.java
@@ -1,24 +1,16 @@
 public class class1 {
     public static void main(String[] args) {
 
-     //Deneme 1 V1
-     int age = 22;
+        //Deneme 1 V1
+        int age = 22;
 
-     //Deneme 2 V2
+        //Deneme 2 V2
 
         double num = 20.20;
 
+        //deneme V3
 
-
-
-
-
-
-
-
-
-
-
+        float cellnum = 0.000001f;
 
 
     }
